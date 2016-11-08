166c42ef86c64b037a3d3fb9ecc9188396023cb1
rollingw <current082002@yahoo.com>
2016-11-08 07:55:31 -0600

    update

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,1 +1,8 @@
-reducer_books.js
+export default function() {
+	return [
+		{ title: 'Javascript: The Good Parts'},
+		{ title: 'Harry Potter'},
+		{ title: 'The Dark Tower'},
+		{ title: 'Elequent Ruby'},
+	]
+}
